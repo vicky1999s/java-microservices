dd9d8160cfc1989caa7856894ab998a864a7b80c
Vigneshwaran <vigneshwaran.surulivel@gmail.com>
2022-06-19 19:08:27 +0530

    initial commit

---
+++ currency-conversion-service/currency-conversion-service/src/main/java/com/vicky/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -0,0 +1,13 @@
+package com.vicky.microservices.currencyconversionservice;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "currency-exchange")
+public interface CurrencyExchangeProxy {
+	
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyConversion retriveExchangeValue(@PathVariable String from, @PathVariable String to);
+	
+}
---
+++ currency-exchange-service/currency-exchange-service/src/main/java/com/vicky/microservices/currencyexchangeservice/CurrencyExchangeRepo.java
@@ -0,0 +1,7 @@
+package com.vicky.microservices.currencyexchangeservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CurrencyExchangeRepo extends JpaRepository<CurrencyExchange, Long>{
+	CurrencyExchange findByFromAndTo(String from, String to);
+}
